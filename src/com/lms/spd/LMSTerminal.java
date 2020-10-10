a6e4e5e9211f045cb67c59f1ac2de686e75cbbec
Oleksiy Surovtsev <joleksiysurovtsev@gmail.com>
2020-10-10 13:41:37 +0300

    #1 Partially worked out menu 4 Menu navigation Display of bibliography

--- src/com/lms/spd/LMSTerminal.java
+++ src/com/lms/spd/LMSTerminal.java
@@ -10,7 +10,7 @@ public class LMSTerminal {
 
     public static void startLMS() throws IOException {
         System.out.println("\u001B[34m" + "Welcome to the " + "\"\u001B[32mL\u001B[35mM\u001B[31mS\u001B[34m" + "\"" + ": learning management system\n" + "\u001B[0m");
-        getMainMenu();
+        showMainMenu();
 
         switch (reader.readLine()) {
             case "1":
@@ -42,9 +42,9 @@ public class LMSTerminal {
                     }
                 } while (flag);
                 break;
+
             case "3":
                 System.out.println("Please enter the number of the lecture you want to delete");
-
                 boolean flagDelete;
                 do {
                     flagDelete = false;
@@ -52,7 +52,7 @@ public class LMSTerminal {
                     ls.removeLecture(number);
                     System.out.println("Lecture deleted want to delete another one? " +
                             "if yes then enter " + "\u001B[31m" + "\"+\" " + "\u001B[0m" +
-                            "if you return to the menu " + "\u001B[32m" + "\"-\"" + "\u001B[0m" + ""+ " or \u001B[31m" + "\"EXIT\"" + "\u001B[0m" + " end the program");
+                            "if you return to the menu " + "\u001B[32m" + "\"-\"" + "\u001B[0m" + "" + " or \u001B[31m" + "\"EXIT\"" + "\u001B[0m" + " end the program");
                     String deleteOrNot = reader.readLine();
                     if (deleteOrNot.equalsIgnoreCase("+")) {
                         flagDelete = true;
@@ -68,17 +68,23 @@ public class LMSTerminal {
                     }
                 } while (flagDelete);
                 break;
+            case "4":
+                chooseLecture();
+                break;
+            case "0":
+                System.exit(0);
+                break;
             default:
-                throw new IllegalStateException("Unexpected value: " + reader.readLine());
+                System.out.println("\u001B[35m" + "No such menu item exists try again" + "\u001B[0m");
+                workingFourthMenu();
         }
 
     }
 
-
     /**
      * Method of calling the terminal main menu
      */
-    private static void getMainMenu() {
+    private static void showMainMenu() {
         System.out.println("Please make your choice from the offered options\n"
                 + "1. Display all available lectures (number and title)\n"
                 + "2. Add a new lecture\n"
@@ -88,17 +94,8 @@ public class LMSTerminal {
     }
 
     /**
-     * menu for working with the selected lecture
+     * method for navigation: return to main navigation menu
      */
-    private static void choiceOfLecture() {
-        System.out.println("введите номер лекции, информацию о которой вы хотите посмотреть если передумали для выхода в меню введите " + "\u001B[31m" + "0" + "\u001B[0m");
-        System.out.println("Какие дальнейшие дейсвия?");
-        System.out.println("1. --> просмотреть список литературы");
-        System.out.println("2. --> добавить новую (название / ссылка)");
-        System.out.println("3. --> удалить литературу");
-        System.out.println("4. --> выйти в меню на уровень выше");
-    }
-
     private static void lectureOutputMenu() throws IOException {
         switch (reader.readLine().toUpperCase()) {
             case "0":
@@ -114,5 +111,63 @@ public class LMSTerminal {
         }
     }
 
+    /**
+     * lecture selection and lecture submenu
+     */
+    static void chooseLecture() throws IOException {
+        System.out.println("Enter the number of the lecture, " +
+                "information about which you want to see if you change your mind to exit to the menu enter " + "\u001B[32m" + "0" + "\u001B[0m");
+        int numbOfLecture = Integer.parseInt(reader.readLine());
+        //устанавливаем номер
 
+        if (numbOfLecture == 0) {
+            startLMS();
+        }
+
+        if (numbOfLecture > ls.getArrLectureLength()) {
+            System.out.println("\u001B[31m" + "There is no such lecture" + "\u001B[0m" + "\nlet's try again");
+            chooseLecture();
+        } else {
+            ls.setSelectedLecture(numbOfLecture - 1);
+            System.out.println("Selected lecture " + "\u001B[35m" + "\"" + ls.getNameSelectedLecture() + "\"" + "\u001B[0m");
+            System.out.println("What are the next actions?");
+
+            showFourthMenu();
+            workingFourthMenu();
+        }
+    }
+
+    static void workingFourthMenu() throws IOException {
+        switch (reader.readLine()) {
+            case "1":
+                ls.getListLit(ls.getSelectedLecture());
+                System.out.println("_______________________");
+                System.out.println("what do we do with the bibliography");
+                showFourthMenu();
+                workingFourthMenu();
+                break;
+            case "2":
+                System.out.println("add new literature (title / link)");
+                break;
+            case "3":
+                System.out.println("remove");
+                break;
+            case "4":
+                chooseLecture();
+                break;
+
+            default:
+                System.out.println("\u001B[35m" + "No such menu item exists try again" + "\u001B[0m");
+                workingFourthMenu();
+                break;
+        }
+    }
+
+    static void showFourthMenu() {
+        System.out.println("1. --> view the list of literature");
+        System.out.println("2. --> add new (title / link)");
+        System.out.println("3. --> remove literature");
+        System.out.println("4. --> go to the menu one level higher");
+        System.out.println("____________________");
+    }
 }
--- src/com/lms/spd/ListOfLectures.java
+++ src/com/lms/spd/ListOfLectures.java
@@ -1,8 +1,17 @@
 package com.lms.spd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ListOfLectures {
 
-    private static String[][] lecture = {
+    private int selectedLecture;
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    private String[][] lecture = {
             {/*0*/"Java Core", "Head First Java", "Java. Руководство для начинающих"},
             {/*1*/"Core Java API", "Java. Полное руководство", "Java SE 9. Базовый курс", "Java. Методы программирования"},
             {/*2*/"Class design", "Effective Java", "Философия Java"},
@@ -13,19 +22,19 @@ public class ListOfLectures {
     /**
      * The method prints the list of lectures to the console
      */
-    public void getLectureList() {
+     void getLectureList() {
         for (int i = 0; i < lecture.length; i++) {
             int numbOfLecture = i + 1;
             String nameOfLecture = lecture[i][0];
             System.out.println("Lecture №" + numbOfLecture + ". " + nameOfLecture);
         }
-        System.out.println(" ");
+        System.out.println("______________________");
     }
 
     /**
      * Adds a new lecture to an array
      */
-    public void addLecture(String lectureAddName) {
+     void addLecture(String lectureAddName) {
         String[][] newArray = new String[lecture.length + 1][];
         // из    начиная с  в новый  начиная с  по предпоследний (последний пустой)
         System.arraycopy(lecture, 0, newArray, 0, newArray.length - 1);
@@ -37,8 +46,8 @@ public class ListOfLectures {
 
     /**
      * Remove lecture from array
-     * */
-    public void removeLecture(int lectureRemove) {
+     */
+     void removeLecture(int lectureRemove) {
         String[][] newArray1 = new String[lectureRemove - 1][];
         System.arraycopy(lecture, 0, newArray1, 0, newArray1.length);
 
@@ -51,5 +60,60 @@ public class ListOfLectures {
 
         lecture = newArray3;
     }
+
+
+    /*
+      methods block of working with a specific lecture
+      */
+
+
+    /**
+     * the method throws the number of the selected lecture into the variable
+     */
+      void setSelectedLecture(int selected) throws IOException {
+        //проверить есть ли такая лекция
+        if (selected > getArrLectureLength()) {
+            System.out.println("lectures with this number do not exist in total in the base of " + getArrLectureLength() + " lectures.");
+            setSelectedLecture(Integer.parseInt(reader.readLine()));
+        } else {
+            selectedLecture = selected;
+        }
+    }
+
+    /**
+     * the method gets from the variable the number of the selected lecture
+     */
+     int getSelectedLecture() {
+        return selectedLecture;
+    }
+
+    /**
+     * the method returns the number of lectures
+     */
+      int getArrLectureLength() {
+        return lecture.length;
+    }
+
+    /**
+     * the method returns the name of the lecture that we selected earlier
+     */
+      String getNameSelectedLecture() {
+        int x = getSelectedLecture();
+        return lecture[x][0];
+    }
+
+    /**
+     * the method returns a list of references from the previously selected lecture
+     */
+        void getListLit(int numbLecture) {
+        if (lecture[numbLecture].length == 1) {
+            System.out.println("\u001B[31m" + "Lecture is empty, first add literature to it" + "\u001B[0m");
+        } else {
+            for (int i = 1; i < lecture[numbLecture].length; i++) {
+                System.out.println(i + ". " + lecture[numbLecture][i]);
+            }
+        }
+    }
+
 
 }
