8d984da52f93b0462c92d60bf3d1decde34cbc4c
Oleksiy Surovtsev <joleksiysurovtsev@gmail.com>
2020-10-22 17:17:25 +0300

    #3 Doing Add test LectureTest, LiteratureTest, LMSTerminalTest

---
+++ src/test/java/com/lms/spd/LMSTerminalTest.java
@@ -0,0 +1,31 @@
+package com.lms.spd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LMSTerminalTest {
+    LMSTerminal lmsTerminal = new LMSTerminal();
+    private LectureService lectureService;
+
+    @BeforeEach
+    void createLectureService() {
+        lectureService = new LectureService();
+        Lecture[] lectures = {
+                new Lecture(1, "BufferedReader."),
+                new Lecture(2, "Writes text to."),
+                new Lecture(3, "Core Java API"),
+        };
+        lectureService.setLectures(lectures);
+    }
+
+    @Test
+    void startLMS() {
+    }
+
+    @Test
+    void testCheckNumberLecture() {
+        assertEquals(true, lmsTerminal.checkNumberLecture(1));
+    }
+}
---
+++ src/test/java/com/lms/spd/LiteratureTest.java
@@ -0,0 +1,43 @@
+package com.lms.spd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LiteratureTest {
+
+    @Test
+    void getName() {
+        Literature testLit = new Literature("Test");
+        assertEquals("Test", testLit.getName());
+    }
+
+    @Test
+    void setName() {
+        Literature testLit = new Literature("Test");
+        testLit.setName("New name");
+        assertEquals(new Literature("New name"), testLit);
+    }
+
+    @Test
+    void testToString() {
+        Literature testLit = new Literature("Test");
+        assertEquals(" Book= Test", testLit.toString());
+    }
+
+    @Test
+    void testEquals() {
+        Literature testLit = new Literature("Test");
+        Literature testLit2 = new Literature("Test");
+        Literature testLit3 = new Literature("Test2");
+        assertTrue(testLit.equals(testLit2));
+        assertFalse(testLit.equals(testLit3));
+    }
+
+    @Test
+    void testHashCode() {
+        Literature testLit = new Literature("Test");
+        int hashCode = testLit.hashCode();
+        assertEquals(hashCode, testLit.hashCode());
+    }
+}
