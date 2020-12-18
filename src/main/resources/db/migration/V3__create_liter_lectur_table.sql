172194b7a21c8751684999f74339a6739f3b3288
Oleksiy Surovtsev <joleksiysurovtsev@gmail.com>
2020-12-19 01:46:30 +0200

    update table #flyway

--- src/main/resources/db/migration/V3__create_liter_lectur_table.sql
+++ src/main/resources/db/migration/V3__create_liter_lectur_table.sql
@@ -1,14 +1,12 @@
-CREATE TABLE literature_to_lectures (
-                              id SERIAL PRIMARY KEY,
-                              literature_id INTEGER NOT NULL,
-                              lecture_id INTEGER NOT NULL,
-                              CONSTRAINT "FK_user_id" FOREIGN KEY (literature_id)
-                                  REFERENCES "literature" ("lit_id"),
-                              CONSTRAINT "FK_site_id" FOREIGN KEY (lecture_id)
-                                  REFERENCES "lectures" ("lect_id")
+CREATE TABLE literature_to_lectures
+(
+    lect_id integer NOT NULL,
+    lit_id integer  NOT NULL,
+    PRIMARY KEY (lect_id , lit_id ),
+    FOREIGN KEY (lect_id) REFERENCES lectures,
+    FOREIGN KEY (lit_id ) REFERENCES literature
 );
 
-CREATE UNIQUE INDEX "UI_literature_to_lectures_literature_id_lectures_id"
-    ON "literature_to_lectures"
-        USING btree
-        ("literature_id", "lecture_id");
+
+
+
